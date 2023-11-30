860affd015b9ad8e746c7bf3acf885225ac13b87
peterhenryd <peterhenryd@gmail.com>
2023-11-30 00:00:08 -0500

    Grasping at straws

--- src/repo.ts
+++ src/repo.ts
@@ -5,13 +5,7 @@ export function parseRepo(core: Core, slug: string): Repo | null {
         return null;
     }
 
-    const pieces = slug.split("/");
-
-    if (pieces.length != 2) {
-        core.setFailed("Your repository slug should be formatted \"{owner}/{name}\".");
-    }
-
-    const [owner, name] = pieces;
+    const [owner, name] = slug.split("/");
 
     return { owner, name }
 }
